e385ff1306225d770bdf7d6fc45a34da980899cb
libin <libin1@sugon.com>
2019-09-27 17:14:39 +0800

    为什么softmax要使用指数函数

--- 测试代码段/softmax_exp.py
+++ 测试代码段/softmax_exp.py
@@ -9,6 +9,8 @@ a = np.array([10011, 10010, 10009])
 # a_exp = np.exp(a) / np.sum(np.exp(a))
 # print(a_exp)
 
+# 为什么softmax要使用指数函数：https://segmentfault.com/a/1190000017320763?utm_source=tag-newest
+
 c = np.max(a)
 a_exp_c = np.exp(a - c) / np.sum(np.exp(a - c))
 print(a_exp_c)
